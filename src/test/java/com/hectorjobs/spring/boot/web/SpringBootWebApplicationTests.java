b5a44ba9dab085950f72b5b9ca651357a9b1629d
Hector Valdes <hectorvaldes0808@gmail.com>
2023-02-11 12:44:09 -0600

    initial commit

---
+++ src/test/java/com/hectorjobs/spring/boot/web/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hectorjobs.spring.boot.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
